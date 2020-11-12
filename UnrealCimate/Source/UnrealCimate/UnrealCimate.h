6b5f3ef44ffbd0aa5304e0e0d7002097dede5844
tpaloma <72208944+tpaloma@users.noreply.github.com>
2020-11-12 11:18:35 +0100

    Update UnrealCimate.h

--- UnrealCimate/Source/UnrealCimate/UnrealCimate.h
+++ UnrealCimate/Source/UnrealCimate/UnrealCimate.h
@@ -2,5 +2,71 @@
 
 #pragma once
 
+//Contains a GameModeBase class which I called city and represents our "world", it will not exist on the interface but defines the rules and settings for the game.
+//For now, we put all the structure subclasses without using the unreal editor; for each object which will appear in the 3d world, we will however need to use the unreal class wiard
+
 #include "CoreMinimal.h"
 
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "City.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT2_API ACity : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
+
+class Structure : public MYPROJECT2_API ACity {
+public:
+    double cost, location, age, type;
+
+}
+
+class Production : public Structure {
+public:
+    double input, output, efficiency;
+    int employment;
+}
+
+class Housing : public Structure {
+public:
+
+}
+
+class PublicServices : public Structure {
+public:
+    double input, output, efficiency;
+    int employment;
+}
+
+class Factory : public Production {
+
+
+}
+
+class Shop : public Production {
+    //get attributes from production : employment i don't remember how to do that
+    //constructor :
+    Shop(double employment);
+}
+
+
+class Energy : public Factory {
+    double energy_output;
+    double environmental_impact();
+}
+
+class Solar : public Energy {
+    double energy_output;
+    double environmental_impact() //redefined don't use same as the one in energy
+}
+
+class Nuclear : public Energy {
+    double energy_output;
+    double environmental_impact() //redefined don't use same as the one in energy
+}
