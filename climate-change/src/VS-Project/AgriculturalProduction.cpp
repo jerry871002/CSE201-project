399b6f560ec0b5448447903a9f3a4cb626844ebb
tpaloma <paloma.thevenet@polytechnique.edu>
2020-11-19 10:36:58 +0100

    Update AgriculturalProduction.cpp

--- climate-change/src/VS-Project/AgriculturalProduction.cpp
+++ climate-change/src/VS-Project/AgriculturalProduction.cpp
@@ -1,1 +1,43 @@
 #include "AgriculturalProduction.h"
+#include <Math.hpp>
+#include <GodotGlobal.hpp>
+
+using namespace godot;
+
+void AgriculturalProduction::_register_methods()
+{
+	register_method((char*)"_process", &AgriculturalProduction::_process);
+	register_method((char*)"_input", &AgriculturalProduction::_input);
+	register_method((char*)"_ready", &AgriculturalProduction::_ready);
+}
+
+void AgriculturalProduction::_init()
+{
+
+}
+
+void AgriculturalProduction::_process(float delta)
+{
+
+}
+
+void AgriculturalProduction::_input(InputEvent* e)
+{
+
+}
+
+void AgriculturalProduction::_ready()
+{
+
+}
+
+
+AgriculturalProduction::AgriculturalProduction()
+{
+
+}
+
+AgriculturalProduction::~AgriculturalProduction()
+{
+
+}
