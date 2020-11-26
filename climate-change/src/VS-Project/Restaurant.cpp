e66caa1f54dea2e3d60e25a8bf8788ce346fe5a4
Eliott-Sarrey <eliott.sarrey@gmail.com>
2020-11-26 09:59:24 +0100

    Pointer

--- climate-change/src/VS-Project/Restaurant.cpp
+++ climate-change/src/VS-Project/Restaurant.cpp
@@ -36,20 +36,17 @@ void Restaurant::change_income_by_percent(float factor ) {
 	income *= factor;
 }
 
-void Restaurant::_init()
-{
+void Restaurant::_init() {
 
 }
 
-void Restaurant::_process(float delta)
-{
+void Restaurant::_process(float delta) {
 
 }
 
-void Restaurant::_input(Input* e)
-{
+void Restaurant::_input(Input* e) {
 	if (e->get_class() == "InputEventMouseButton" && Clickable) {
-		if (((InputEventMouseButton*)e)->is_pressed()) {   //And button index = ?
+		if (((InputEventMouseButton*)e)->is_pressed()) {		//And button index = ?
 			PanelsOn = (PanelsOn == false);
 			this->GetPanels()->set("visible", PanelsOn);
 		}
@@ -64,24 +61,17 @@ void Restaurant::_ready()
 
 void godot::Restaurant::_on_Area_mouse_entered()
 {
-
-	Clickable = true;
-	//this->GetPanels()->set("visible", true);
-
-	Input* i = Input::get_singleton();
-	// CHANGE MOUSE CURSOR
+	//Clickable = true;											//this->GetPanels()->set("visible", true);
+	Input* i = Input::get_singleton();							// CHANGE MOUSE CURSOR
 	i->set_default_cursor_shape(i->CURSOR_POINTING_HAND);
-	
 }
+
+
 
 void godot::Restaurant::_on_Area_mouse_exited()
 {
-
-	Clickable = false;
-	//this->GetPanels()->set("visible", false);
-	Input* i = Input::get_singleton();
-	// CHANGE MOUSE CURSOR
+	Clickable = false;											//this->GetPanels()->set("visible", false);
+	Input* i = Input::get_singleton();							// CHANGE MOUSE CURSOR
 	i->set_default_cursor_shape(i->CURSOR_ARROW);
-
 }
 
