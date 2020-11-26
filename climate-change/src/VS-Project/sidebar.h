65538f8c0836f66c3179e2e92018bdba17f737de
Arthur Chambat <arthurchambat@gmail.com>
2020-11-26 09:37:02 +0100

    sidebar

--- climate-change/src/VS-Project/sidebar.h
+++ climate-change/src/VS-Project/sidebar.h
@@ -3,3 +3,17 @@
 #include <core/Godot.hpp>
 #include <TextureRect.hpp>
 #include "Root.h"
+
+namespace godot {
+	class Menu : public TextureRect {
+		GODOT_CLASS(Menu, TextureRect)
+	public:
+		Menu();
+		~Menu();
+
+		static void _register_methods();
+		void _init();
+
+		void _on_TextureButton_pressed(Variant body);
+	};
+}
