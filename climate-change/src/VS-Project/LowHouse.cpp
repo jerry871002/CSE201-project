c54980c1067b80be66d75e24debfa5a81eb0b39b
saga_david <saga.k.david@gmail.com>
2020-11-26 11:41:58 +0100

    lowhouse

--- climate-change/src/VS-Project/LowHouse.cpp
+++ climate-change/src/VS-Project/LowHouse.cpp
@@ -4,40 +4,45 @@
 
 using namespace godot;
 
-void LowHouse::_register_methods()
-{
+void LowHouse::_register_methods(){
 	register_method((char*)"_process", &LowHouse::_process);
 	register_method((char*)"_input", &LowHouse::_input);
 	register_method((char*)"_ready", &LowHouse::_ready);
 }
 
-void LowHouse::_init()
-{
+void LowHouse::_init(){
 
 }
 
-void LowHouse::_process(float delta)
-{
+void LowHouse::_process(float delta){
 
 }
 
-void LowHouse::_input(InputEvent* e)
-{
+void LowHouse::_input(InputEvent* e){
 
 }
 
-void LowHouse::_ready()
-{
+void LowHouse::_ready(){
 
 }
 
-
-LowHouse::LowHouse()
-{
+LowHouse::LowHouse() {
+//attributes from structure class
+cost = 100000; //cost to build a new house (value for a low cost house, 1000€ / m^2)
+energyuse = 68.49 kWh; //25000kWh per year i.e. 13.69 kWh per day (from heating and all )
+maintenance = 0.1765; //cost in euros per kWh
+CO2_output = 0,0065; //6.5g per kWh
+building_time = 140; //in average, building a house takes about 140 days
+satisfaction = 3; //assuming we are on a scale from 0 to 10
+//attributes special to this class
+insulation = 0.5; //I'm taking this attribute to be a percentage of insulation capacity, which is here low at first 
 
 }
 
-LowHouse::~LowHouse()
-{
-
+LowHouse::~LowHouse(){
 }
+
+void LowHouse::simulate_step(double days) {
+	
+}
+
--- climate-change/src/VS-Project/LowHouse.h
+++ climate-change/src/VS-Project/LowHouse.h
@@ -1,5 +1,5 @@
 #include "obj.h"
-
+//all these numbers are for a house of 100m^2
 class LowHouse : public Housing {
 public:
 	LowHouse();
@@ -10,3 +10,14 @@ public:
 	void _input(InputEvent* e);
 	void _ready();
 }
+
+void simulate_step(double days); // updates every attribute directly as a function of time (days since beginning of the game)
+    double insulation;
+    
+	
+
+	// policies (constructor creates subcritical plant of 38% efficiency) :
+	bool solar_panel(); // improve efficiency to supercritical type of plant (42% energy converted to electricity)
+	bool double_glazing(); // improve insulation
+    
+};
