3c06f0fb8be1ba9ae78dd4e25f0b2eee30179c59
samsahsch <sam.sahsch@gmail.com>
2020-11-30 14:28:01 +0100

    Update main_loop.cpp

--- climate-change/src/VS-Project/main_loop.cpp
+++ climate-change/src/VS-Project/main_loop.cpp
@@ -1,4 +1,3 @@
-#pragma once
 #include <core/Godot.hpp>
 #include <iostream>
 #include <cstdlib>
@@ -6,7 +5,11 @@
 #include "Restaurant.h"
 #include "obj.h"
 #include "City.h"
-#include<windows.h>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 
 
 
@@ -25,18 +28,18 @@ int main()
 		Restaurant rest = Restaurant();
 		Struc* struc_pointer = &rest;
 		c.add_building(struc_pointer);
-		Sleep(200);
+		sleep(200);
 
 		// advance by 3 days before rerunning through the loop 
 		c.simulation();
 		std::cout << c.return_game_date() << std::endl;
 		std::cout << "city income is " << c.return_income() << std::endl;
-		Sleep(200);
+		sleep(200);
 
 		c.simulation();
 		std::cout << c.return_game_date() << std::endl;
 		std::cout << "city income is " << c.return_income() << std::endl;
-		Sleep(200);
+		sleep(200);
 
 		c.simulation();
 		std::cout << c.return_game_date() << std::endl;
