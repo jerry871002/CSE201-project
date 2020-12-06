3af270961b9a0a36b2d9a2b001c2c78ab9ee8795
annavlasova19 <anna.vlasova@polytechnique.edu>
2020-12-07 01:07:57 +0300

    transport update

--- climate-change/src/VS-Project/TransportTest.cpp
+++ climate-change/src/VS-Project/TransportTest.cpp
@@ -35,27 +35,35 @@ Transport electricCar = Transport(0);
 Transport bigCar = Transport(1);
 Transport car = Transport(2);
 Transport collectionCar = Transport(3);
+Transport bike = Transport(4);
 Transport bus = Transport(6);
 electricCar.simulate_step(days);
 bigCar.simulate_step(days);
 car.simulate_step(days);
 collectionCar.simulate_step(days);
 bus.simulate_step(days); 
+bike.simulate_step(days);
 std::cout<<"Electric car: co2 emission "<<electricCar.CO2Emission<<", age "<<electricCar.age<<", energy use "<<electricCar.energyUse<< ", fuel input "<<electricCar.fuelInput<<", maintenance "<<electricCar.maintenance<<", passengers "<<electricCar.passengers<<std::endl;
 std::cout<<"Big car: co2 emission "<<bigCar.CO2Emission<<", age "<<bigCar.age<< ", fuel input "<<bigCar.fuelInput<<", maintenance "<<bigCar.maintenance<<", passengers "<<bigCar.passengers<<std::endl;
 std::cout<<"Car: co2 emission "<<car.CO2Emission<<", age "<<car.age<< ", fuel input "<<car.fuelInput<<", maintenance "<<car.maintenance<<", passengers "<<car.passengers<<std::endl;
 std::cout<<"Bus: co2 emission "<<bus.CO2Emission<<", age "<<bus.age<< ", fuel input "<<bus.fuelInput<<", maintenance "<<bus.maintenance<<", passengers "<<bus.passengers<<std::endl;
+std::cout<<"Bike: co2 emission "<<bike.CO2Emission<<", age "<<bike.age<< ", fuel input "<<bike.fuelInput<<", maintenance "<<bike.maintenance<<", passengers "<<bike.passengers<<std::endl;
+std::cout<<"Collection car: co2 emission "<<collectionCar.CO2Emission<<", age "<<collectionCar.age<< ", fuel input "<<collectionCar.fuelInput<<", maintenance "<<collectionCar.maintenance<<", passengers "<<collectionCar.passengers<<std::endl;
 }
+
 void test_constructor() {
     Transport electricCar = Transport(0);
     Transport bigCar = Transport(1);
     Transport car = Transport(2);
     Transport collectionCar = Transport(3);
+    Transport bike = Transport(4);
     Transport bus = Transport(6);
     std::cout<<"Electric car: capacity "<<electricCar.capacity<<" , occupancy rate "<<electricCar.occupancyRate<<", cost "<<electricCar.cost<<" ,building time "<<electricCar.buildingTime<<", satisfaction "<<electricCar.satisfaction<<std::endl;
     std::cout<<"Bus: capacity "<<bus.capacity<<" , occupancy rate "<<bus.occupancyRate<<" cost"<<bus.cost<<" ,building time "<<bus.buildingTime<<", employment "<<bus.employment<<", satisfaction "<<bus.satisfaction<<std::endl;
     std::cout<<"Car: capacity "<<car.capacity<<" , occupancy rate "<<car.occupancyRate<<" cost"<<car.cost<<" ,building time "<<car.buildingTime<<", satisfaction "<<car.satisfaction<<std::endl;
     std::cout<<"Big car: capacity "<<bigCar.capacity<<" , occupancy rate "<<bigCar.occupancyRate<<" cost"<<bigCar.cost<<" ,building time "<<bigCar.buildingTime<<", satisfaction "<<bigCar.satisfaction<<std::endl;
+    std::cout<<"Bike: capacity "<<bike.capacity<<" , occupancy rate "<<bike.occupancyRate<<" cost"<<bike.cost<<" ,building time "<<bike.buildingTime<<", satisfaction "<<bike.satisfaction<<std::endl;
+    std::cout<<"Collection car: capacity "<<collectionCar.capacity<<" , occupancy rate "<<collectionCar.occupancyRate<<" cost"<<collectionCar.cost<<" ,building time "<<collectionCar.buildingTime<<", satisfaction "<<collectionCar.satisfaction<<std::endl;
     std::cout << "Constructor Works " << std::endl;
 }
 
