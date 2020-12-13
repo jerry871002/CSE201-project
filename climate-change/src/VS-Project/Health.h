905838b485f3a6f25f0e4e1542c8665d9980b40f
annavlasova19 <anna.vlasova@polytechnique.edu>
2020-12-14 01:13:09 +0300

    healthcare start

--- climate-change/src/VS-Project/Health.h
+++ climate-change/src/VS-Project/Health.h
@@ -1,7 +1,11 @@
-#include "obj.h"
-#include <core/Godot.hpp>
 #pragma once
-#include <StaticBody.hpp>
+
+#include "City.h"
+#include "obj.h"
+
+#include <core/Godot.hpp>
+#include <KinematicBody.hpp>
+
 namespace godot {
 class Health : public Infrastructure, public StaticBody {
 		GODOT_CLASS(Health, StaticBody)
@@ -12,6 +16,9 @@ class Health : public Infrastructure, public StaticBody {
 		void _init();
 		void _process(float delta);
 		void _input(InputEvent* e);
-		void _ready();
+		void _ready();	
+		double patientsAdmitted, successRate;
+		int hospitalType;
+
 	};
 };
