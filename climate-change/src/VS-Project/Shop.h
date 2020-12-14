aba4d287f15d035e14cc3685a979bd2e0d199822
Antoine de Mascarel <antoinedemascarel@gmail.com>
2020-12-14 15:48:56 +0100

    Shop file created

---
+++ climate-change/src/VS-Project/Shop.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "obj.h"
+
+#include <core/Godot.hpp>
+#include <StaticBody.hpp>
+#include <MeshInstance.hpp>
+#include <Input.hpp>
+#include <InputEventMouse.hpp>
+#include <InputEventMouseMotion.hpp>
+#include <InputEventMouseButton.hpp>
+
+
+namespace godot{
+    class Shop : public Structure {
+        GODOT_SUBCLASS(Shop, Structure)
+        
+    public:
+        Shop();
+        ~Shop();
+
+        int shopType;       //Tells us what type of the object is
+        bool shopStatus;   //True if open, False if closed
+        double employment;
+
+        // Will have type: 0 - Empty. 1 - Restaurant. 2 - Small Shop. 3 - Mall
+        void initialize(int shopTypeInput);
+        
+        double get_satisfaction();
+        double get_co2emissions();
+        double get_energyuse();
+        double get_environmentalcost();
+
+        //Attributes used for restaurant:
+        int restaurantType;
+        double energyUsePerSize;
+        double diningSize;
+        double averageWage;
+        bool firstYearShock = false;
+
+
+        void simulate_step(double days);
+    };
+}
