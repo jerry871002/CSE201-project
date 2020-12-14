f5b1ae2dd45fc884d9fbd55c38b73d4701dff569
tpaloma <paloma.thevenet@polytechnique.edu>
2020-12-14 22:00:51 +0100

    Energy updated

--- climate-change/src/VS-Project/Energy.cpp
+++ climate-change/src/VS-Project/Energy.cpp
@@ -18,7 +18,7 @@ Energy::~Energy() {
 }
 
 /// <summary>
-/// POWERPLANT CLASS
+/// NUCLEAR POWERPLANT CLASS
 /// </summary>
 
 NuclearPowerPlant::NuclearPowerPlant() {
@@ -102,5 +102,123 @@ void Windmill::simulate_step(double days)
 	/*
 	if age >= 7300{
 		// 20 years is the average lifetime of a windmill, it then has to be replaced by a new one or destroyed
+	}*/
+}
+
+/// <summary>
+/// GEOTHERMAL POWERPLANT CLASS
+/// </summary>
+
+GeothermalPowerPlant::GeothermalPowerPlant()
+{
+	srand((int)time(0));
+	double probability = (rand() % (10));
+	if (probability < 2) {
+		cost = 7E6; // when drilling the wells (accounts for 2 million euros approx) ther is a 20% chance of failure 
+	}
+	else {
+		cost = 5E6; // cost in euros to build a new plant (plus all the research needed)
+	}
+	age = 0;
+	satisfaction = 8; // on scale of 10
+	CO2Emission = 0.09; // kg of CO2 emitted per kWh
+	H2SEmission = 8.2E-5; //kg of H2S emitted per kWh
+	CH4Emission = 1.6E-5; //kg of CH4 emitted per kWh
+	NH3Emission = 1.7E-5; //kg of NH3 emitted per kWh
+	maintenance = 0.08; //maintenace and working cost in euros per kWh
+	buildingTime = 8; // years needed to build a new plant (plus research needed)
+	environmentalCost = 0.0015; // environmental and health costs in euros per kWh
+	employment = 50; // average number of employees linked to one plant
+}
+
+GeothermalPowerPlant::~GeothermalPowerPlant() {}
+
+void GeothermalPowerPlant::simulate_step(double days)
+{
+	age += days;
+	energyPerDay = 32800; //kWh produced by in one day
+	energyOutput += energyPerDay * days; // total kWh produced by a standard plant 
+	maintenance += 0.08 * energyPerDay * days;
+	if (age >= 3650) {
+		maintenance += 0.08 * 0.25; // after 10 years the maintenance and working costs increase by 1/4
+	}
+	if (age >= 7300) {
+		maintenance += 0.08; // after 20 years the maintenance and working costs double
+	}
+	CO2Emission += 0.09 * energyPerDay * days;
+	NH3Emission += 1.7E-5 * energyPerDay * days;
+	CH4Emission += 1.6E-5 * energyPerDay * days;
+	H2SEmission += 8.2E-5 * energyPerDay * days;
+
+	environmentalCost = 0.0015 * energyPerDay * days;
+
+	/*
+	if age >= 10950{
+		// 30 years is the average lifetime of a geothermal plant, it then has to be replaced by a new plant or different power plant
+	}*/
+}
+
+
+/// <summary>
+/// COAL POWERPLANT CLASS
+/// </summary>
+
+CoalPowerPlant::CoalPowerPlant()
+{
+	age = 0; // age of the building in days
+	coal = 4.06E-4; // tons of coal needed to produce 1 kWh
+	maintenance = 0.05; //maintenace and working cost in euros per kWh
+	employment = 800; // approximate number of employees in 1 plant 
+	satisfaction = 4; // on scale of 10
+	CO2Emission = 0.868; // kg of CO2 emitted per kWh
+	SO2Emission = 0.00152; // kg of SO2 emitted per kWh
+	NOxEmission = 8.49E-4; // kg of NOx emitted per kWh
+	ashOutput = 0.0619; // kg of ash produced per kWh
+	mercuryEmission = 1.137E-8; // kg of mercury emitted per kWh
+	cost = 3E9; // cost in euros to build a new plant
+	buildingTime = 8; // years needed to build a new plant
+	environmentalCost = 0.06; // environmental and health costs in euros per kWh
+}
+
+
+CoalPowerPlant::~CoalPowerPlant()
+{
+
+}
+
+void CoalPowerPlant::simulate_step(double days)
+{
+	age += days;
+	energyPerDay = 9589041; //kWh produced by standard plant in one day, we consider it to be the same for every plant in our simulation
+	energyOutput += energyPerDay * days; // total kWh produced by a standard plant 
+	if (efficiency_supercritical() == true) {
+		energyPerDay = 9589041 * (1 - 0.04);
+		maintenance += 0.054 * energyPerDay * days;
+	}
+	if (efficiency_cogeneration() == true) {
+		energyPerDay = 9589041 * (1 - 0.09);
+		maintenance += 0.058 * energyPerDay * days;
+	}
+	else {
+		maintenance += 0.05 * energyPerDay * days;
+	}
+	if (age >= 3650) {
+		maintenance += 0.05 * 0.25; // after 10 years the maintenance and working costs increase by 1/4
+	}
+	if (age >= 10950) {
+		maintenance += 0.05; // after 30 years the maintenance and working costs double
+	}
+
+	coal += 4.06E-4 * energyPerDay * days;
+	CO2Emission += 0.768 * energyPerDay * days;
+	SO2Emission += 0.00152 * energyPerDay * days;
+	NOxEmission += 8.49E-4 * energyPerDay * days;
+	ashOutput += 0.0619 * energyPerDay * days;
+	mercuryEmission += 1.137E-8 * energyPerDay * days;
+	environmentalCost = 0.06 * energyPerDay * days;
+
+	/*
+	if age >= 18250{
+		// 50 years is the average lifetime of a coal fired plant, it then has to be replaced by a new coal plant or different power plant
 	}*/
 }
--- climate-change/src/VS-Project/Energy.h
+++ climate-change/src/VS-Project/Energy.h
@@ -5,27 +5,37 @@
 
 namespace godot {
 
-    /// <summary>
-    /// SUPER CLASS ENERGY
-    /// </summary>
-	
-    class Energy : public Structure {
-        GODOT_SUBCLASS(Energy, Structure)
+	/// <summary>
+	/// SUPER CLASS ENERGY
+	/// </summary>
 
-    public:
-        Energy();
-        ~Energy();
+	class Energy : public Structure {
+		GODOT_SUBCLASS(Energy, Structure)
+
+	public:
+		Energy();
+		~Energy();
 
 		//inputs:
+		//Nuclear plant :
 		double naturalUranium = 0;
 		double fissileMaterial = 0;
+		double requiredLand = 0;
+		double coal = 0;
 		//outputs:
 		double nuclearWaste = 0;
 		double radiation = 0;
 		double energyPerDay = 0;
 		double energyOutput = 0;
+		double H2SEmission = 0;
+		double CH4Emission = 0;
+		double NH3Emission = 0;
+		double SO2Emission = 0;
+		double NOxEmission = 0;
+		double ashOutput = 0;
+		double mercuryEmission = 0;
 
-    };
+	};
 
 	/// <summary>
 	/// NUCLEAR POWER PLANT CLASS
@@ -43,7 +53,7 @@ namespace godot {
 	/// <summary>
 	/// WINDMILL CLASS
 	/// </summary>
-	
+
 	class Windmill : public Energy {
 		GODOT_SUBCLASS(Windmill, Energy)
 	public:
@@ -51,13 +61,38 @@ namespace godot {
 		~Windmill();
 
 		void _process(float delta);
-		
+
 		//This class gives the functionnalities for one single onshore windmill, 
 		//the sim team can use it in order to simulate a wind farm of the size it chooses
 		void simulate_step(double days); //updates attribute by adding to their previous values as a function of time (days since last step)
-		double requiredLand;
 		double turnSpeed;
 	private:
 		double rot = 0;
+	};
+
+	/// <summary>
+	/// COAL POWER PLANT CLASS
+	/// </summary>
+
+	class CoalPowerPlant : public Energy {
+		GODOT_SUBCLASS(CoalPowerPlant, Energy)
+	public:
+		CoalPowerPlant();
+		~CoalPowerPlant();
+
+		void simulate_step(double days); //updates attribute by adding to their previous values as a function of time (days since last step)
+	};
+
+	/// </summary>
+	/// GEOTHERMAL POWER PLANT CLASS
+	/// </summary>
+
+	class GeothermalPowerPlant : public Energy {
+		GODOT_SUBCLASS(GeothermalPowerPlant, Energy)
+	public:
+		GeothermalPowerPlant();
+		~GeothermalPowerPlant();
+
+		void simulate_step(double days); //updates attribute by adding to their previous values as a function of time (days since last step)
 	};
 }
