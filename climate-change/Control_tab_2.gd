83f174dfb5ee9e89b9b2929ee3fe286635b46dc6
lucabonengel <72190529+lucabonengel@users.noreply.github.com>
2020-12-16 02:01:37 +0100

    fixing bugs with graphs

--- climate-change/Control_tab_2.gd
+++ climate-change/Control_tab_2.gd
@@ -9,6 +9,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$BarChart2D.plot()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
