ef3e7f015d932b4c44fa68ea5eae3042e5589aaf
annavlasova19 <anna.vlasova@polytechnique.edu>
2020-12-17 04:20:57 +0300

    agricultural production basics

--- climate-change/src/VS-Project/AgriculturalProduction.h
+++ climate-change/src/VS-Project/AgriculturalProduction.h
@@ -1,8 +1,11 @@
 #pragma once
+
+#include "City.h"
+#include "obj.h"
+#include "Production.h"
+
 #include <core/Godot.hpp>
 #include <KinematicBody.hpp>
-#include "obj.h"
-#include <StaticBody.hpp>
 
 namespace godot {
 	class AgriculturalProduction : public Production {
@@ -17,5 +20,18 @@ namespace godot {
 		void _process(float delta);
 		void _input(InputEvent* e);
 		void _ready();
+		void simulate_step(double days);
+		double get_satisfaction();
+		double get_co2emissions();
+		double get_energyuse();
+		double get_environmentalcost();
+		double waterConsumption; // per day 
+		int agricultureType; // 0 - wheat, 1 - vegetables, 2 - meat
+		bool pesticide; // true if pesticides are used
+		bool GMO; // true if GMO is used
+		bool fertilizer; // true if fertilizer is used
+		double size; //size of the field in m^2
+		double fertility; //per m^2 output
+		double production; //how output much per day
 	};
 }
