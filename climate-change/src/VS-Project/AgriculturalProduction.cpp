14412b37cf57e10342931ba068a490ff5c1ddacc
annavlasova19 <anna.vlasova@polytechnique.edu>
2020-12-17 15:28:53 +0300

    agricultural production cpp

--- climate-change/src/VS-Project/AgriculturalProduction.cpp
+++ climate-change/src/VS-Project/AgriculturalProduction.cpp
@@ -1,52 +1,74 @@
 #include "AgriculturalProduction.h"
-#include <Math.hpp>
-#include <GodotGlobal.hpp>
-#include <iostream>
 
+#include <random>
 using namespace godot;
 
-String godot::AgriculturalProduction::class_name()
-{
+String godot::AgriculturalProduction::class_name(){
 	return "AgriculturalProduction";
 }
 
-void AgriculturalProduction::_register_methods()
-{
+void AgriculturalProduction::_register_methods(){
 	register_method((char*)"_init", &AgriculturalProduction::_init);
 	register_method((char*)"_process", &AgriculturalProduction::_process);
 	register_method((char*)"_input", &AgriculturalProduction::_input);
 	register_method((char*)"_ready", &AgriculturalProduction::_ready);
 }
 
-void AgriculturalProduction::_init()
-{
+void AgriculturalProduction::_init(){
 
 }
 
-void AgriculturalProduction::_process(float delta)
-{
+void AgriculturalProduction::_process(float delta){
 
 }
 
-void AgriculturalProduction::_input(InputEvent* e)
-{
+void AgriculturalProduction::_input(InputEvent* e){
 
 }
 
-void AgriculturalProduction::_ready()
-{
+void AgriculturalProduction::_ready(){
 
 }
 
 
-AgriculturalProduction::AgriculturalProduction()
-{
-
+AgriculturalProduction::AgriculturalProduction(){
+	int agricultureType = rand()%3;
+	agriculture_type(agricultureType);
 }
 
-AgriculturalProduction::~AgriculturalProduction()
-{
+AgriculturalProduction::AgriculturalProduction(int type){
+	agriculture_type(type);
+}
 
+void AgriculturalProduction::agriculture_type(int type){
+	agricultureType= type; // 0 - wheat, 1 - meat, 2 - vegetables
+	switch(agricultureType){
+		case(0):{ // wheat
+
+		break;
+		}
+
+	}
+}
+void AgriculturalProduction::simulate_step(double days){
+	switch(agricultureType){
+		case(0):{ // wheat
+
+		break;
+		}
+}
+}
+double AgriculturalProduction::get_satisfaction(){
+	return this->satisfaction;
+}
+double AgriculturalProduction::get_co2emissions(){
+	return this->CO2Emission;
+}		
+double AgriculturalProduction::get_energyuse(){
+	return this->energyUse;
+}
+double AgriculturalProduction::get_environmentalcost(){
+	return 0;
 }
 
 // void AgriculturalProduction::test() {
