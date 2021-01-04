564926477951d9a028d6becaa1b8ceda3da49b10
Antoine de Mascarel <antoinedemascarel@gmail.com>
2021-01-04 15:00:16 +0100

    Debugging done

--- climate-change/src/VS-Project/Shop.h
+++ climate-change/src/VS-Project/Shop.h
@@ -79,6 +79,12 @@ namespace godot {
         ~SmallShop();
         virtual String get_object_type() { return "SmallShop"; };
 
+        String class_name();
+
+        int smallShopType;
+        double averageWage;
+
+        void simulate_step(double days);
     };
 
     //####################################   Mall   ################################
@@ -91,6 +97,7 @@ namespace godot {
         ~Mall();
         virtual String get_object_type() { return "Mall"; };
 
+        String class_name();
     };
 
 }
