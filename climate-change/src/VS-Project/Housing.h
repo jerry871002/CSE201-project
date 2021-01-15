97fc35fef1fa72444c14dcf46b693cc365c622c4
Vincent-Sietses <vincent.sietses@gmail.com>
2021-01-15 15:30:11 +0100

    thing

--- climate-change/src/VS-Project/Housing.h
+++ climate-change/src/VS-Project/Housing.h
@@ -46,11 +46,9 @@ namespace godot {
 		int rooftopWindTurbineAge = 0;
 		double normalGenerator(double mean, double stdDev);
 
-
-		//  why are these guys not used ??
-		//double set_max_income();
-		//double set_min_income();
-		//double get_max_income();
+		double set_max_income();
+		double set_min_income();
+		double get_max_income();
 
 		double get_min_income();
 		double get_solar_panel_age();
