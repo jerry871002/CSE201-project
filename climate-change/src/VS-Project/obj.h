6d1e47ff22a4777d41f1c0c23d1342c0b42ac536
samsahsch <author.samuel@samsahsch.com>
2021-01-16 09:34:55 +0100

    other small changes

--- climate-change/src/VS-Project/obj.h
+++ climate-change/src/VS-Project/obj.h
@@ -59,7 +59,6 @@ namespace godot {
 
         virtual void simulate_step(double days);
         bool updatable{ false };
-        //void test_update();
 
         real_t MenuSize{ 400 };
         real_t InfoBoxWidth{ 300 };
